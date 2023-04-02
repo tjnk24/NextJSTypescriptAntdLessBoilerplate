c6e6d6345fe50451c1c73769be96456526a500a4
tjnk24 <tjnk24@gmail.com>
2023-04-02 16:45:41 +0400

    404 page

---
+++ pages/404.ts
@@ -0,0 +1,1 @@
+export {default} from '__pages/Page404';
---
+++ src/pages/Page404/containers/Page404/Page404.tsx
@@ -0,0 +1,27 @@
+import {Button} from 'antd';
+import React from 'react';
+
+import {routeManager} from '__utils/routing/routeManager';
+
+import {root, returnToUsersButton} from './Page404.module.less';
+
+const Page404 = () => {
+    const onReturnToUsersClick = () => {
+        routeManager.goToUsers();
+    };
+
+    return (
+        <div className={root}>
+            Not Found
+
+            <Button
+                className={returnToUsersButton}
+                onClick={onReturnToUsersClick}
+            >
+                Return to users page
+            </Button>
+        </div>
+    );
+};
+
+export default Page404;
